852b142487ac58179b8cb699e3d7754f98edbbea
Christian Johnson <cartian4@gmail.com>
2024-04-12 10:29:08 -0400

    Add comments

--- src/main/java/com/christianjcodes/App.java
+++ src/main/java/com/christianjcodes/App.java
@@ -16,6 +16,7 @@ public class App implements Runnable, ActionListener {
     JButton submit;
     JTextField input;
 
+    // Checks if a letter is included in the provided word/array
     public static boolean containsLetter(char a, char[] arrB) {
 
         for (int i = 0; i < arrB.length; i++) {
@@ -26,6 +27,7 @@ public class App implements Runnable, ActionListener {
         return false;
     }
 
+    // Determines the number of times a character is used in a word
     public static int charUsage(char a, char[] arrB) {
 
         int count = 0;
@@ -110,6 +112,7 @@ public class App implements Runnable, ActionListener {
 
             String inputWord = userWord.toLowerCase();
 
+            // Create an array and add the characters from the input word to it
             char[] usableChars = new char[inputWord.length()];
 
             for (int i = 0; i < inputWord.length(); i++) {
@@ -117,8 +120,10 @@ public class App implements Runnable, ActionListener {
                 usableChars[i] = letter;
             }
 
+            // Create an empty HashSet to contain the found anagram words
             HashSet<String> narrowedList = new HashSet<>();
 
+            // Check each word in wordList against the input word to find anagrams
             for (String a : wordList) {
 
                 boolean wrongLetter = false;
@@ -126,11 +131,14 @@ public class App implements Runnable, ActionListener {
                 // Check for any letters that aren't in inputWord
                 for (int i = 0; i < a.length(); i++) {
                     char letter = a.charAt(i);
-                    if (!containsLetter(letter, usableChars)) {
+                    if (!containsLetter(letter, usableChars)) { // If containsLetter returns false, we have a wrong letter
                         wrongLetter = true;
                     }
                 }
 
+                /* If the length is the same and there are no wrong letters, create an array that
+                stores the characters of the current word
+                */
                 if (!wrongLetter && a.length() == usableChars.length) {
                     char[] aChars = new char[a.length()];
 
@@ -141,6 +149,10 @@ public class App implements Runnable, ActionListener {
 
                     int match = 0;
 
+                    /* Compare the input word to the current word to check firstly if each word uses each letter
+                    the same number of times (accounting for multiple letter usage), and then increment a variable
+                    that will be used to determine if the character count is the same with multiple letters.
+                    */
                     for (int i = 0; i < usableChars.length; i++) {
                         for (int j = 0; j < aChars.length; j++) {
                             if (charUsage(usableChars[i], usableChars) == charUsage(aChars[j], aChars)) {
@@ -152,6 +164,7 @@ public class App implements Runnable, ActionListener {
                         }
                     }
 
+                    // Add the word to the final list if matches, and if it's not the original input word
                     if (match == inputWord.length() && !a.equals(inputWord)) {
                         narrowedList.add(a);
                     }
